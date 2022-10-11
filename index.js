df001d26b363fe86d175786e8b2c049f921cafef
Matheusp007-226 <83099808+Matheusp007-226@users.noreply.github.com>
2022-10-11 09:02:43 -0300

    Atualização da API para receber mensagens do webhook para recebimento de boletos

--- index.js
+++ index.js
@@ -65,37 +65,65 @@ let resp_json = {nome: "Matheus pimentel", idade: 26};
             } else {
               console.log('Email enviado: ' + info.response);
             }
-          });
-        
+          });   
     
   })
 
   app.put('/webhook', function(req, res) {
 
-    if(req.body.tipo == 'transacao_documento'){
+        if(req.body.tipo == 'transacao_documento'){
 
-        console.log('Documento '+ req.body.documento[0].nome+ ' vinculado a transação')
+            console.log('Documento '+ req.body.documento[0].nome+ ' vinculado a transação')
 
-        console.log(req.body);
+            console.log(req.body);
 
-        res.json(req.body);
+            res.json(req.body);
 
-    }else if(req.body.tipo == "conta_digital"){
+        }else if(req.body.tipo == "conta_digital"){
 
-        console.log('APROVAÇÃO DE CONTA!');
+            console.log('APROVAÇÃO DE CONTA!');
 
-        console.log(req.body);
+            console.log(req.body);
 
-        res.json(req.body);
-    }
+            res.json(req.body);
 
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email enviado: ' + info.response);
+        }else if(req.body.tipo == 'recebimento_boleto'){
+
+          console.log('Pagamento e recebimento de boleto!');
+
+          console.log(req.body);
+
+          res.json(req.body);
+
+          const transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true, // true for 465, false for other ports
+            auth: {
+                user: "sac@sgnsistemas.com.br",
+                pass: "inova*@78#q)hRj)p-071"
+            },
+            tls: { rejectUnauthorized: false }
+          });
+        
+          const mailOptions = {
+            from: 'sac@sgnsistemas.com.br',
+            to: 'matheus.007brasil@gmail.com',
+            subject: 'E-mail enviado sobre o recebimento de boleto de pagamento!',
+            text: 'Vamos pagar as contas? Receba este boleto!'
+          };
+  
+        
         }
-      });
+
+      
+        transporter.sendMail(mailOptions, function(error, info){
+              if (error) {
+                console.log(error);
+              } else {
+                console.log('Email enviado: ' + info.response);
+              }
+          });
 
   })
 
